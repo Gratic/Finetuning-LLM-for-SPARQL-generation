7e6114a6971c2e28a2a7392ce2d6ec3e17fd1cd1
Gratic <blairodu74@yahoo.fr>
2023-12-05 21:59:39 +0100

    feat: SimpleSentencePlaceholder + test

---
+++ libwikidatallm/SentencePlaceholder.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import List
+from .Pipeline import PipelineStep
+
+class SentencePlaceholder(ABC):
+    
+    @abstractmethod
+    def annotate(self, sentence: str, entities: List[str], properties: List[str]) -> str:
+        pass
+    
+class SimpleSentencePlaceholder(SentencePlaceholder, PipelineStep):
+    def annotate(self, sentence: str, entities: List[str], properties: List[str]) -> str:
+        for i, entity in enumerate(entities):
+            sentence = sentence.replace(entity, f"[entity {i}]")
+        
+        for i, property in enumerate(properties):
+            sentence = sentence.replace(property, f"[property {i}]")
+        
+        return sentence
+    
+    def execute(self, context: dict):
+        context["annotated_sentence"] = self.annotate(context["row"], context["extracted_entities"], context["extracted_properties"])    
--- libwikidatallm/__init__.py
+++ libwikidatallm/__init__.py
@@ -3,3 +3,4 @@ from .PipelineFeeder import PipelineFeeder, SimplePipelineFeeder
 from .TemplateLLMQuerySender import TemplateLLMQuerySender
 from .LLMConnector import LLMConnector, LLMResponse, ServerConnector, LlamaCPPConnector, CTransformersConnector
 from .EntityExtractor import EntityExtractor, LLMEntityExtractor
+from .SentencePlaceholder import SentencePlaceholder, SimpleSentencePlaceholder
