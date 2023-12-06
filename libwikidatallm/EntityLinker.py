3ab206e510eed4ae4b6e5eb76b122d0aa5f7ed81
Gratic <blairodu74@yahoo.fr>
2023-12-06 16:20:50 +0100

    feat: EntityLinker

---
+++ libwikidatallm/EntityLinker.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from typing import List
+from .Pipeline import PipelineStep
+from .EntityFinder import WikidataAPI
+
+class EntityLinker(ABC):
+    @abstractmethod
+    def link_entities(self, extracted: List[str]):
+        pass
+    
+class PropertyLinker(ABC):
+    @abstractmethod
+    def link_properties(self, extracted: List[str]):
+        pass
+    
+class FirstWikidataEntityLinker(EntityLinker, PropertyLinker, PipelineStep):
+    def __init__(self) -> None:
+        self.wikidataAPI = WikidataAPI()
+    
+    def link_entities(self, extracted: List[str]):
+        entities_linked = []
+        for entity in extracted:
+            result = self.wikidataAPI.find_entities(entity)
+            entities_linked.append((entity, result[0]))
+        return entities_linked  
+
+    def link_properties(self, extracted: List[str]):
+        properties_linked = []
+        for entity in extracted:
+            result = self.wikidataAPI.find_properties(entity)
+            properties_linked.append((entity, result[0]))
+        return properties_linked 
+    
+    def execute(self, context: dict):
+        context["linked_entities"] = self.link_entities(context["extracted_entities"])
+        context["linked_properties"] = self.link_entities(context["extracted_properties"])
