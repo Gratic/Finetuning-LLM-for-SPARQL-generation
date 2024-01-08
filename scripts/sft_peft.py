dd75119ca1b0fe0897e5062e6de2bf5005690dd9
Alexis <alexis.strappazzon@insa-lyon.fr>
2024-01-08 20:04:26 +0000

    feat: working finetuning script

--- scripts/sft_peft.py
+++ scripts/sft_peft.py
@@ -1,20 +1,33 @@
+import torch
 from datasets import load_dataset
 from peft import LoraConfig
-from transformers import TrainingArguments
-from trl import SFTTrainer, AutoModelForCausalLMWithValueHead
+from transformers import TrainingArguments, AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer
+from trl import SFTTrainer
+from accelerate import Accelerator
 
-def prompt_generator(example):
-    output_texts = []
-    batch_size = len(example['input'])
-    for i in range(batch_size):
-        for j in range(len(example['input'][i])):
-            prefix = "Answer this question with a SPARQL query:\n"
-            
-            text = f"{prefix}{example['input'][i][j]}\n{example['target'][i]}"
-            output_texts.append(text)
-    return output_texts
-
+dataset = load_dataset("pandas", data_files="./outputs/finetune_dataset.pkl")
 model_id = "mistralai/Mistral-7B-Instruct-v0.2"
+
+# https://github.com/huggingface/trl/blob/main/examples/research_projects/stack_llama/scripts/supervised_finetuning.py
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+def format_prompt(example):
+    prefix = ""
+    
+    text = f"{tokenizer.bos_token}[INST] Answer this question with a SPARQL query: {example['input']} [/INST] {example['target']}{tokenizer.eos_token}"
+    return text
 
 lora_config = LoraConfig(
     r=16,
@@ -24,28 +37,47 @@ lora_config = LoraConfig(
     task_type="CAUSAL_LM",
 )
 
-pretrained_model = AutoModelForCausalLMWithValueHead.from_pretrained(
-    model_id,
-    peft_config=lora_config,
+bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
-    load_in_8bit=False
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
 )
+
+pretrained_model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    quantization_config=bnb_config,
+    device_map={"": Accelerator().process_index}
+)
+
+# https://medium.com/@parikshitsaikia1619/mistral-mastery-fine-tuning-fast-inference-guide-62e163198b06
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer.padding_side = "right"
+tokenizer.pad_token = tokenizer.unk_token
+
+pretrained_model.config.pad_token_id = tokenizer.pad_token_id
+
+print_trainable_parameters(pretrained_model)
 
 training_args = TrainingArguments(
-                    bf16=True,
-                    output_dir="./outputs"
+    bf16=True,
+    output_dir="./outputs",
+    optim="adamw_bnb_8bit",
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=4,
+    dataloader_pin_memory=True,
+    dataloader_num_workers=0,
 )
-
-dataset = load_dataset("pandas", data_files="./outputs/finetune_dataset.pkl")
 
 trainer = SFTTrainer(
     pretrained_model,
     args=training_args,
+    tokenizer=tokenizer,
     train_dataset=dataset["train"],
-    formatting_func=prompt_generator,
+    formatting_func=format_prompt,
     max_seq_length=4096,
     peft_config=lora_config,
-    packing=True
+    packing=True,
 )
 
 trainer.train()
