fbbc71aa9c76abee8ea05e35b249c56bae57a439
Alexis <alexis.strappazzon@insa-lyon.fr>
2024-01-14 10:58:03 +0000

    feat: merge adapter script

---
+++ scripts/merge_adapters.py
@@ -0,0 +1,22 @@
+from peft import PeftModel
+from transformers import AutoModelForCausalLM
+import argparse
+
+
+def merge_model_with_adapters(base_model_name, adapter_model_name, output_file):
+    model = AutoModelForCausalLM.from_pretrained(base_model_name)
+    model = PeftModel.from_pretrained(model, adapter_model_name)
+
+    model = model.merge_and_unload()
+    model.save_pretrained(output_file)
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="Model + Adapter merger",
+                                     description="Merge a model with its adapaters.")
+    parser.add_argument('model-path', required=True, type=str)
+    parser.add_argument('adapter-path', required=True, type=str)
+    parser.add_argument('output-path', default="merged_model", type=str)
+    
+    args = parser.parse_args()
+    
+    merge_model_with_adapters(args.model_path, args.adapter_path, args.output_path)
