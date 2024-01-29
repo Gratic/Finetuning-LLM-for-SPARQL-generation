7a7cec2ae0629e33922f9faa0300e462258124ec
Gratic <blairodu74@yahoo.fr>
2024-01-29 17:37:18 +0100

    feat: test for execute_queries.py

--- execute_queries.py
+++ execute_queries.py
@@ -5,6 +5,38 @@ import time
 import argparse
 import os
 
+def send_query_to_api(query, api, timeout_limit, num_try):
+    response = None
+    while num_try > 0 and response == None and not is_query_empty(query):
+        try:
+            print(f"| Calling API... ", end="", flush=True)
+            sparql_response = api.execute_sparql(query, timeout=timeout_limit)
+            response = sparql_response.bindings if sparql_response.success else sparql_response.data
+                
+        except HTTPError as inst:
+            if inst.response.status_code == 429:
+                retry_after = int(inst.response.headers['retry-after'])
+                print(f"| Retry-after: {retry_after} ", end="", flush=True)
+                time.sleep(retry_after + 1)
+                num_try -= 1
+            else:
+                print(f"| Exception occured ", end="", flush=True)
+                response = "exception: " + str(inst)
+        except Timeout:
+            response = "timeout"
+            print(f"| Response Timeout ", end="", flush=True)
+        except Exception as inst:
+            print(f"| Exception occured ", end="", flush=True)
+            response = "exception: " + str(inst)
+    return response if response != None else "exception: too many retry-after"
+
+def is_query_empty(query :str) -> bool:
+    query = query.strip()
+    return query is None or query == "" or len(query) == 0
+
+def can_add_limit_clause(query :str) -> bool:
+    return (not is_query_empty(query) and not "COUNT" in query and not "LIMIT" in query)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="SPARQL Queries Executor",
                                     description="Execute queries on Wikidata's SPARQL endpoint")
@@ -24,6 +56,7 @@ if __name__ == "__main__":
 
     df_dataset = pd.read_parquet(dataset_path, engine="fastparquet")
     df_dataset['execution'] = df_dataset.apply(lambda x: None, axis=1)
+    df_dataset['executed_query'] = df_dataset.apply(lambda x: None, axis=1)
 
     api = WikidataAPI()
     
@@ -31,46 +64,23 @@ if __name__ == "__main__":
     for (i, query) in df_dataset[args.column_name].items():
         print(f"row {str(num_processed)}/{len(df_dataset)} ".ljust(15), end="", flush=True)
         response = None
-        is_empty = False
         
-        if query is None or query == "" or len(query) == 0:
-            response = ""
-            is_empty = True
+        if is_query_empty(query):
+            response = "exception: query is empty"
             print(f"| Query is empty ", end="", flush=True)
+        else:
+            if do_add_limit and can_add_limit_clause(query):
+                query += f"\nLIMIT {answer_limit}"
             
-        
-        num_try_left = 3
-        
-        if not is_empty and do_add_limit and "COUNT" in query and not "LIMIT" in query:
-            query += f"\nLIMIT {answer_limit}"
-        
-        while num_try_left > 0 and response == None and not is_empty:
-            try:
-                print(f"| Calling API... ", end="", flush=True)
-                sparql_response = api.execute_sparql(query, timeout=timeout_limit)
-                response = sparql_response.bindings if sparql_response.success else sparql_response.data
-                
-            except HTTPError as inst:
-                if inst.response.status_code == 429:
-                    retry_after = int(inst.response.headers['retry-after'])
-                    print(f"| Retry-after: {retry_after} ", end="", flush=True)
-                    time.sleep(retry_after + 1)
-                    num_try_left -= 1
-                else:
-                    print(f"| Exception occured ", end="", flush=True)
-                    num_try_left -= 1
-                    response = "exception: " + str(inst)
-            except Timeout:
-                response = "timeout"
-                num_try_left -= 1
-                print(f"| Response Timeout ", end="", flush=True)
-            except Exception as inst:
-                print(f"| Exception occured ", end="", flush=True)
-                response = "exception: " + str(inst)
-                num_try_left -= 1
+            
+            response = send_query_to_api(query=query,
+                                    api=api,
+                                    timeout_limit=timeout_limit,
+                                    num_try=3)
         print(f"| done.", flush=True)
         
         df_dataset.at[i, 'execution'] = str(response)
+        df_dataset.at[i, 'executed_query'] = query
         num_processed += 1
 
     df_dataset.to_parquet(os.path.join(args.output, f"{args.save_name}.parquet.gzip"), engine="fastparquet", compression="gzip")
---
+++ scriptstests/ExecuteQueriesTest.py
@@ -0,0 +1,111 @@
+from typing import Any
+import unittest
+from execute_queries import can_add_limit_clause, is_query_empty, send_query_to_api
+from libwikidatallm.EntityFinder import WikidataAPI
+import requests
+
+class QueryGenerator():
+    def __init__(self) -> None:
+        pass
+    
+    def __call__(self, id, *args: Any, **kwds: Any) -> Any:
+        if id == 0:
+            return "A normal query"
+        elif id == 1:
+            return ""
+        elif id == 2:
+            return " "
+        elif id == 3:
+            return "                       "
+        elif id == 4:
+            return "A query containing a LIMIT clause"
+        elif id == 5:
+            return "A query COUNT -ing something"
+        elif id == 6:
+            return "A query containing a LIMIT clause and a COUNT -ing clause."
+        raise ValueError("This id is not supported.")
+
+class MockResponse():
+    def __init__(self, success, msg) -> None:
+        self.success = success
+        self.msg = msg
+    
+    @property
+    def bindings(self):
+        return None if not self.success else self.msg
+    
+    @property
+    def data(self):
+        return None if self.success else self.msg
+
+class MockAPI(WikidataAPI):
+    def __init__(self, base_url: str = "https://www.wikidata.org/w/api.php") -> None:
+        super().__init__(base_url)
+    
+    def execute_sparql(self, query: str, timeout: int = None):
+        if query == "valid":
+            return MockResponse(True, "success")
+        elif query == "retry-after":
+            response = requests.Response()
+            response.headers['retry-after'] = '1'
+            response.status_code = 429
+            raise requests.exceptions.HTTPError(request=None, response=response)
+        elif query == "400Error":
+            response = requests.Response()
+            response.status_code = 400
+            raise requests.exceptions.HTTPError(response=response)
+        elif query == "timeout":
+            raise requests.exceptions.Timeout()
+            
+        return ValueError("This query is not supported.")
+
+class ExecuteQueries(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.query = QueryGenerator()
+        self.api = MockAPI()
+    
+    def test_is_query_empty_normal(self):
+        self.assertFalse(is_query_empty(self.query(0)))
+    
+    def test_is_query_empty_empty_0(self):
+        self.assertTrue(is_query_empty(self.query(1)))
+    
+    def test_is_query_empty_empty_1(self):
+        self.assertTrue(is_query_empty(self.query(2)))
+    
+    def test_is_query_empty_empty_2(self):
+        self.assertTrue(is_query_empty(self.query(3)))
+    
+    def test_can_add_limit_clause_valid(self):
+        self.assertTrue(can_add_limit_clause(self.query(0)))
+        
+    def test_can_add_limit_clause_empty_0(self):
+        self.assertFalse(can_add_limit_clause(self.query(1)))
+        
+    def test_can_add_limit_clause_empty_1(self):
+        self.assertFalse(can_add_limit_clause(self.query(2)))
+        
+    def test_can_add_limit_clause_empty_2(self):
+        self.assertFalse(can_add_limit_clause(self.query(3)))
+        
+    def test_can_add_limit_clause_contains_LIMIT(self):
+        self.assertFalse(can_add_limit_clause(self.query(4)))
+        
+    def test_can_add_limit_clause_contains_COUNT(self):
+        self.assertFalse(can_add_limit_clause(self.query(5)))
+        
+    def test_can_add_limit_clause_contains_COUNT_and_LIMIT(self):
+        self.assertFalse(can_add_limit_clause(self.query(6)))
+        
+    def test_send_query_to_api_valid(self):
+        self.assertEqual("success", send_query_to_api("valid", self.api, None, 3))
+        
+    def test_send_query_to_api_retry_after(self):
+        self.assertEqual("exception: too many retry-after", send_query_to_api("retry-after", self.api, None, 3))
+    
+    def test_send_query_to_api_HTTPError(self):
+        self.assertEqual("exception: ", send_query_to_api("400Error", self.api, None, 3))
+        
+    def test_send_query_to_api_timeout(self):
+        self.assertEqual("timeout", send_query_to_api("timeout", self.api, None, 3))
