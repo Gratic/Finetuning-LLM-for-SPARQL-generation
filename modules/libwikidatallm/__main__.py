cfa73b1a469944574cd49b65cd87307812fb46e7
Gratic <blairodu74@yahoo.fr>
2024-03-02 19:50:19 +0100

    fix: modules import

--- modules/libwikidatallm/__main__.py
+++ modules/libwikidatallm/__main__.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+sys.path.append(Path("modules").absolute().__str__())
+
 from .EntityExtractor import LLMEntityExtractor
 from .EntityLinker import FirstWikidataEntityLinker
 from .LLMConnector import LlamaCPPConnector, vLLMConnector, PeftConnector
